c17894bbd73910a48b689564df41c5edea93d757
Harsh Vardhan Pandey <157698602+harshvp072@users.noreply.github.com>
2025-05-15 18:11:25 +0530

    project started

---
+++ Study/src/main/java/FocusFlow/Study/FocusFlowApplication.java
@@ -0,0 +1,13 @@
+package FocusFlow.Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusFlowApplication.class, args);
+	}
+
+}
---
+++ Study/src/test/java/FocusFlow/Study/FocusFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package FocusFlow.Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
